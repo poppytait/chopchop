39766ee025afa70f444594c40b5860d5044b9991
Poppy Tait <poppytait@Poppys-MacBook-Air.local>
2018-12-17 22:45:56 +0100

    add to favourites

--- routes/recipe.js
+++ routes/recipe.js
@@ -4,22 +4,49 @@ const router = express.Router();
 const mongoose = require('mongoose');
 
 const User = require('../models/user');
+const Recipe = require('../models/recipe');
 
 const { isLoggedIn } = require('../helpers/middleware');
 
-router.post('/:recipeID', isLoggedIn(), (req, res, next) => {
-    console.log('recipe ID', req.params.recipeID)
-    let recipeID = req.params.recipeID;
+router.post('/', isLoggedIn(), (req, res, next) => {
     console.log('user id', req.session.currentUser)
     let userID = req.session.currentUser._id
-    User.findById(userID)
-        .then(user => {
-            user.favourites.push(recipeID)
-            return user.save()
+    let recipe = req.body;
+
+    const p1 = Recipe.create(recipe)
+        .then(recipeResult => {
+            console.log(recipeResult)
+            return recipeResult._id;
         })
+
+    const p2 = User.findById(userID);
+
+    Promise.all([p1, p2]).then(values => {
+        const recipeId = values[0];
+        const user = values[1];
+        user.favourites.push(recipeId);
+        return user.save()
+    })
         .then(savedUser => {
             res.status(200)
             res.json(savedUser);
+        })
+        .catch(error => {
+            next(error);
+        })
+
+
+})
+
+router.get('/', isLoggedIn(), (req, res, next) => {
+    let userID = req.session.currentUser._id;
+    User.findById(userID)
+        .then(user => {
+            return user.favourites;
+        })
+        .then(favourites => {
+            res.status(200)
+            res.json(favourites);
         })
         .catch(error => {
             next(error);
